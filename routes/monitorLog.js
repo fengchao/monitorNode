93349c650b14e8485238aaaf2ea42c99aae5b7e5
Chao Feng <chaofeng111@gmail.com>
2015-03-09 17:20:36 +0800

    Fix JSlint error. Show current date and sort log by date.

--- routes/monitorLog.js
+++ routes/monitorLog.js
@@ -8,6 +8,14 @@ var Validator = require('validator').Validator;
 
 var MonitorLog = mongoose.model('MonitorLog');
 
+Date.prototype.yyyymmdd = function() {         
+    
+    var yyyy = this.getFullYear().toString();                                    
+    var mm = (this.getMonth()+1).toString(); // getMonth() is zero-based         
+    var dd  = this.getDate().toString();             
+    return yyyy + '-' + (mm[1]?mm:"0"+mm[0]) + '-' + (dd[1]?dd:"0"+dd[0]);
+};  
+
 function validate(message) {
 	var v= new Validator();
 	var errors = [];
@@ -24,11 +32,17 @@ function validate(message) {
 router.get('/show', function (req,res){
 	var message = req.body.message;
 	var errors = validate(message);
-	MonitorLog.find({}, function (err, result) {
+	var currentDay = new Date().yyyymmdd();
+	
+	var searchCallback = function (err, result) {
 		res.render('monitorLog', {
-			result : result
+			result : result,
+			logDate: currentDay
 		});
-	});
+	};
+	
+	//MonitorLog.find({Day : currentDay}).sort('-DateTime').exec(searchCallback);
+	MonitorLog.find().sort('DateTime').exec(searchCallback);
 });
 
 router.get('/import', function(req,res){
@@ -39,20 +53,26 @@ router.get('/import', function(req,res){
 
 var fs = require('fs');
 var Busboy = require('busboy');
+var writeError = function (err) {
+	if (err) {
+		console.log(err);
+	}
+};
+
 router.post('/import', function(req,res){
 	var busboy = new Busboy({headers: req.headers});
 	var targetFile;
 	busboy.on('file', function (fieldname, file, filename) {
 		console.log("Uploading:" + filename);
 		targetFile = '../log_archive/monitor_log/' + filename;
-		fstream = fs.createWriteStream(targetFile);
+		var fstream = fs.createWriteStream(targetFile);
 		file.pipe(fstream);
 	});
 	busboy.on('finish', function() {
 		fs.readFile(targetFile, 'utf8', function (err, data) {
 			var lines = data.toString().split('\n');
 			console.log("Lines: " + lines);
-			for (var i in lines) {
+			for (var i=lines.length-1; i >=0; i--) {
 				console.log("Line[" + i + "]:" + lines[i]);
 				
 				if (lines[i].length < 5) {
@@ -61,18 +81,15 @@ router.post('/import', function(req,res){
 				}
 				var monitorLog = new MonitorLog();
 				var pairs = lines[i].split('&');
-				for (i in pairs) {
+				for (i = pairs.length -1; i>=0; i--) {
 					console.log("Pair:" + pairs[i]);
 					var values = pairs[i].split('=');
 					monitorLog[values[0]] = values[1];
 					if (values[0] === 'DateTime') {
-						monitorLog["Day"] = values[1].slice(0, 10);
+						monitorLog.Day = values[1].slice(0, 10);
 					}
 				}
-				monitorLog.save (function (err) {
-					if (err)
-						console.log(err);
-				});
+				monitorLog.save (writeError);
 			}
 			res.render('monitorImport', {
 				lines: data.toString().split('\n')
