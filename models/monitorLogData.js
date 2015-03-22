8a38abcd7cb6a1956b226c095aee46b68ee4cb17
Chao Feng <chaofeng111@gmail.com>
2015-03-22 20:06:15 +0800

    Rename to models.

---
+++ models/monitorLogData.js
@@ -0,0 +1,28 @@
+var mongoose = require('mongoose');
+
+var MonitorSchema = new mongoose.Schema({
+	IsLenovoPreload: String,
+	OS : String,
+	Language : String,
+	OSBit : String,
+	GUID : String,
+	DateTime : String,
+	SecondXML : String,
+	Day : { type: String, index: true},
+	secondxml : String,
+	LUVersion : String,
+	LogType : String,
+	PackageID : String,
+	MT : String,
+	ErrorCode: String,
+	ReturnCode: String,
+	ImportTime: String
+});
+
+mongoose.model('MonitorLog', MonitorSchema);
+
+mongoose.connect('mongodb://localhost:27017/db', function (error) {
+	if (error) {
+		console.log(error);
+	}
+});
